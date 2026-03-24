59b0106e7aeed9a3365d0d7bce66f3b37fdd073f
NguyenVietNgoc <nguyenvietngoc2006@gmail.com>
2026-03-24 14:13:25 +0700

    bai2

---
+++ src/L1/FirstDialog.java
@@ -0,0 +1,13 @@
+package L1;
+
+import javax.swing.JOptionPane;
+
+import static java.lang.System.exit;
+
+
+public class FirstDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+        System.exit(0);
+    }
+}
