c0984b9bd5d4fa9e924fdcc9f8df1577334a291f
NguyenVietNgoc <nguyenvietngoc2006@gmail.com>
2026-03-24 14:07:08 +0700

    Repair HelloWorld

--- src/L1/HelloWorld.java
+++ src/L1/HelloWorld.java
@@ -2,6 +2,7 @@ package L1;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
     }
 }
